8028ec5cf12dbd22897617804affe619c5a91c75
carolinachm <carolti2013@gmail.com>
2020-12-04 15:29:52 -0300

    criação de novos scripts para testar formulario

--- src/test/java/selenium/TesteCampoTreinamento.java
+++ src/test/java/selenium/TesteCampoTreinamento.java
@@ -1,21 +1,25 @@
 package selenium;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
     @Test
     public void deveAbrirOCampoTreinamento(){
     WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-    //driver.quit();
+    driver.manage().window().maximize();    driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    driver.quit();
     }
     @Test
-    public void devePreencherOsCamposNomeESobrenome(){
+    public void deveInteragirComInputText(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
@@ -24,8 +28,70 @@ public class TesteCampoTreinamento {
 
         Assert.assertEquals("Carolina",driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
         Assert.assertEquals("Mesquita",driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
+        driver.quit();
 
+    }
+    @Test
+    public void deveInteragirComTextArea(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste");
+        Assert.assertEquals("teste", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+        driver.quit();
+    }
+    @Test
+    public void deveInteragirComRadioButao(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+        driver.quit();
+    }
+    @Test
+    public void deveInteragirComCheckBox(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+        driver.quit();
+    }
 
+    @Test
+    public void deveInteragirCombo(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        //combo.selectByIndex(4);
+        //combo.selectByValue("superior");
+        combo.selectByVisibleText("Superior");
+        Assert.assertEquals("Superior", combo.getFirstSelectedOption().getText());
+        driver.quit();
+    }
+    @Test
+    public void deveVerificarValoresCombo(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        List<WebElement> options = combo.getOptions();
+        Assert.assertEquals(8, options.size());
+
+        boolean encontrou = false;
+
+        for(WebElement option: options){
+            if(option.getText().equals("Mestrado")){
+                encontrou = true;
+                break;
+            }
+        }
+        Assert.assertTrue(encontrou);
+        driver.quit();
     }
     
 }
